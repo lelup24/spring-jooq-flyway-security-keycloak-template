b51cb3785c20d1d3b4500a38b37c9c927b11a3e7
Peter Kipper <peter.kipper@arxes-tolina.de>
2024-07-03 16:49:13 +0200

    @EnableMethodSecurity (damit man @PreAuthorize verwenden kann)

--- src/main/java/de/kipper/spring_jooq_flyway_security_keycloak_template/config/SecurityConfig.java
+++ src/main/java/de/kipper/spring_jooq_flyway_security_keycloak_template/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
   private final ErrorHandler errorHandler;
